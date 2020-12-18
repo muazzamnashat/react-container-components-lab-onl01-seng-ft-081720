7ea957f6c0263605c92414b29ae677c636c245f5
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-18 17:03:46 -0500

    commit tree d8b4512a4dcf8082159899bc62c9a9e9e97af34a

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,34 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+export default class LatestMovieReviewsContainer extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            reviews:[]
+        }
+    }
+    componentDidMount = () => {
+        fetch(URL)
+        .then((response) => response.json())
+        .then((response) =>{
+            this.setState({reviews :response.results})
+        })
+    }
+
+    renderMovies =() => {
+        // debugger
+        return this.state.reviews.map(movie =>{
+           return (<MovieReviews data={movie}/>)})
+    }
+
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+                {this.renderMovies()}
+            </div>
+        )
+    }
+}
